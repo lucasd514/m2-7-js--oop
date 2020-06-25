df34c5285b62be026993b86be8218a6be7170d8c
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-06-25 13:16:45 -0400

    1.2

--- workshop/1-Cat/exercise-1.2.js
+++ workshop/1-Cat/exercise-1.2.js
@@ -1,11 +1,15 @@
-// Exercise 1.2
-// ------------
-// Creating a Cat class - Part 2
+class Cat {
+  constructor(name, breed) {
+    this.species = "cat";
+    this.tiredness = 0;
+    this.hunger = 0;
+    this.loneliness = 0;
+    this.happiness = 0;
+    this.name = name;
+    this.breed = breed;
+  }
+}
 
-// A) In the previous exercise, Boots' name is not part of its object.
-// Update the Cat class to accept have a name parameter and a breed parameter and
-// then pass Boots' name and breed as arguments when you instantiate the boots object.
-// When you are done, console.log the boots object
-// and confirm that you have everything in there.
+const boots = new Cat("boots", "siamese");
 
-// By the way, Boots is a Simaese cat...
+console.log(boots);
