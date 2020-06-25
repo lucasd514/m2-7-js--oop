8896f907304e1d15d2c2bebcfcc8fec95814b320
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-06-25 13:12:07 -0400

    1.1

--- workshop/1-Cat/exercise-1.1.js
+++ workshop/1-Cat/exercise-1.1.js
@@ -5,20 +5,19 @@
 // A) Add some properties to our class instances.
 // Objects created via this class should have the following properties:
 
-// {
-//   species: 'cat',
-//   tiredness: 0,
-//   hunger: 0,
-//   loneliness: 0,
-//   happiness: 0,
-// }
-
 class Cat {
-  // Add code here
+  constructor() {
+    this.species = "cat";
+    this.tiredness = 0;
+    this.hunger = 0;
+    this.loneliness = 0;
+    this.happiness = 0;
+  }
 }
 
 // B) Instantiate a cat called 'boots' with the Cat class.
-
+const boots = new Cat();
 // C) What do you see when you console.log(boots)?
-
+console.log(boots); //all the elements
 // D) What if I want to output just boots' species?
+console.log(boots.species); //just cat
