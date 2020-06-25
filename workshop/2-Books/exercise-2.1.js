b6a34f39006bf368cfa5830a0f0b52f86aba28a5
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-06-25 14:01:34 -0400

    q2.1

--- workshop/2-Books/exercise-2.1.js
+++ workshop/2-Books/exercise-2.1.js
@@ -15,6 +15,22 @@
 //
 // Console.log them to verify that all is working.
 
-class Book {}
-
+class Book {
+  constructor(title, genre, author, isread) {
+    this.title = title;
+    this.genre = genre;
+    this.author = author;
+    this.isread = isread || false;
+  }
+}
+const book1 = new Book("Harry Potter", "fantasy", "JK Rowling", "");
+const book2 = new Book(
+  "il gioco della pizza",
+  "food",
+  "Gabriele Bonci",
+  "true"
+);
+const book3 = new Book("Kansas City 1927", "sports", "Simone Conte", "true");
+const book4 = new Book("Boomerang", "finance", "Michael Lews", "true");
+const book5 = new Book("My Life", "politics", "Bill Clinton", true);
 console.log(book1, book2, book3, book4, book5);
