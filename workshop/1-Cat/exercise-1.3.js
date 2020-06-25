a5a9e9c3f636d21a6cbbfbc07e8ed53924972223
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-06-25 13:36:49 -0400

    1.3

--- workshop/1-Cat/exercise-1.3.js
+++ workshop/1-Cat/exercise-1.3.js
@@ -9,16 +9,33 @@
 //    Write methods that will update tiredness, hunger, loneliness, and
 //    happiness.
 
-// HINT: You need to write arrow functions here. () => {}
-// - Write a sleep method that accept hours and reduces tiredness by 5 times
-//   that number. So if the cat has a tiredness of `50`, and sleeps for 10
-//   hours, their tiredness will be reduced to 0 (10 * 5).
-// - Write an eat method that accept number of kibbles and reduces hunger by 1/5
-//   that number.
-// - Write an play method that accept minutes and reduces loneliness by 3 times
-//   that number.
-// - the happiness property should be modified all of the above methods as well.
-//
-// You decide how much sleep, eat, and play affects your cat's happiness.
+class Cat {
+  constructor(name, breed) {
+    this.species = "cat";
+    this.tiredness = 100;
+    this.hunger = 100;
+    this.loneliness = 50;
+    this.happiness = 50;
+    this.name = name;
+    this.breed = breed;
+  }
+  sleep = (hours) => {
+    this.tiredness -= hours * 5;
+    this.happiness += hours * 4;
+  };
 
-// B) call the different methods with appropriate values and then console.log boots to see what happened to the property values.
+  eats = (kibbles) => {
+    (this.hunger -= kibbles * 0.2), (this.happiness += kibbles * 0.05);
+  };
+
+  play = (minutes) => {
+    this.loneliness -= 3 * minutes;
+    this.happiness += minutes * 0.25;
+  };
+}
+
+const boots = new Cat("boots", "siamese");
+boots.play(100);
+boots.sleep(8);
+boots.eats(100);
+console.log(boots); //all are modified with the functions that are rune
